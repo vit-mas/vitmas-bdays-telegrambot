8c2735b54ef5d285663ab8ff7ecd808d6c1662b3
Ashman Sodhi <ashmansodhi@hotmail.com>
2026-04-19 12:58:25 +0530

    Added Documentation

--- test.py
+++ test.py
@@ -14,6 +14,7 @@ client = gspread.authorize(creds)
 
 # Open sheet
 sheet = client.open_by_key("1xDlYvNxdfHD2MzvRqBnAfa5DXU57ic-bRousCXX6JqE").sheet1
+#test
 
 # Fetch data
 data = sheet.get_all_records()
